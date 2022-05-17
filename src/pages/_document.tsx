a1d47713a23d2af91384c655b45b1b666ae1b531
Cleisson <cleissonconstc@gmail.com>
2022-05-17 09:05:45 -0300

    creating the Home page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ export default class MyDocument extends Document {
             crossOrigin="anonymous"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;600;700;900&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700;900&display=swap"
             rel="stylesheet"
           />
           <link rel="shortcut icon" href="favicon.png" type="image/png" />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { MainBox } from "../components/MainBox/";
+import styles from "./styles.module.scss";
 
 const id = "teste";
 
@@ -12,18 +13,48 @@ const Home: NextPage = () => {
         <title>Personal Website</title>
       </Head>
       <MainBox>
-        <div>
-          <Link as={`/experiences`} href="/experiences">
-            <a>Go to Experiences</a>
-          </Link>
-          <br />
-          <Link as={`/projects`} href="/projects">
-            <a>Go to Projects</a>
-          </Link>
-          <br />
-          <Link as={`/project/${id}`} href="project/[id]">
-            <a>Go to project {id}</a>
-          </Link>
+        <div className={styles.content}>
+          <section className={styles.avatarSection}>
+            <div>
+              <img
+                src="/images/backAvatar.png"
+                alt="Blackhole image"
+                className={styles.backImage}
+              />
+              <img
+                src="/images/frontAvatar.jpeg"
+                alt="Profile image"
+                className={styles.frontImage}
+              />
+            </div>
+            <h1>Cleisson de Oliveira Moura</h1>
+            <h3>Software Engineer</h3>
+          </section>
+          <section className={styles.aboutSection}>
+            <h2>{`Hello World! ,👋 I'm Cleisson.`}</h2>
+            <p>
+              {`Specialized in delivering creative solutions in various technical
+              formats for solving real problems. I'm always expanding my
+              knowledge, studying and improving my skills about Technology. I
+              love that!`}
+            </p>
+            <ul>
+              <li>Programming Languages: JavaScript/TypeScript and SQL.</li>
+              <li>
+                Spoken Languages: <span title="Portuguese">🇧🇷</span> -{" "}
+                <span title="English">🇺🇸</span> -{" "}
+                <span title="Spanish">🇪🇸</span>
+              </li>
+            </ul>
+            <div>
+              <Link as={`/experiences`} href="/experiences">
+                <a className={styles.buttonNav}>Experiences</a>
+              </Link>
+              <Link as={`/projects`} href="/projects">
+                <a className={styles.buttonNav}>Projects</a>
+              </Link>
+            </div>
+          </section>
         </div>
       </MainBox>
     </>
